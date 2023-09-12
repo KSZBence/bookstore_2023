df0df2837be887456001157ec8fd6990da99b630
KSZBence <kunszekelybence@gmail.com>
2023-09-12 11:30:59 +0200

    fake adat

--- bookstore/database/seeders/DatabaseSeeder.php
+++ bookstore/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,7 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
+        \App\Models\User::factory(10)->create();
+        \App\Models\Book::factory(10)->create();
+        \App\Models\Copy::factory(10)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
